f2fe281e0dad8e5015de4e41d5dcce6f6b29a71a
Kiet Dang <kdgdang@ucdavis.edu>
2023-01-13 02:04:31 -0800

    added colors to card

--- src/componenets/Card.js
+++ src/componenets/Card.js
@@ -1,7 +1,9 @@
-import React from "react"
+import React from "react";
+import './Card.css';
 const Card=({ data })=>{
     
-    console.log('type of data is: ' + typeof data);
+
+    //console.log('type of data is: ' + typeof data);
     // console.log("data: ", data);
     /*<div>
         {props.data.map((pokemon)=>{
@@ -16,15 +18,74 @@ const Card=({ data })=>{
     //    console.log("card log: " + props.data.name);
     
     const createDivs = function(data){
+
         let arr = []; 
         for(let pokeKey of Object.keys(data)){
+                 let pokemon = data[pokeKey];
+            let backgroundColor=''
+    switch(pokemon.type){
+        case 'grass':
+            backgroundColor='#7AC74C';
+        break;
+        case 'fire':
+            backgroundColor='#EE8130';
+        break;
+        case 'ground':
+            backgroundColor='#E2BF65';
+        break;
+        case 'rock':
+            backgroundColor='#B6A136';
+        break;
+        case 'steel':
+            backgroundColor='#B7B7CE';
+        break;
+        case 'ice':
+            backgroundColor='#96D9D6';
+        break;
+        case 'electric':
+            backgroundColor='#F7D02C';
+        break;
+        case 'dragon':
+            backgroundColor='#6F35FC';
+        break;
+        case 'ghost':
+            backgroundColor='#735797';
+        break;
+        case 'psychic':
+            backgroundColor='#F95587';
+        break;
+        case 'normal':
+            backgroundColor='#A8A77A';
+        break;
+        case 'fighting':
+            backgroundColor='#C22E28';
+        break;
+        case 'poison':
+            backgroundColor='#A33EA1';
+        break;
+        case 'bug':
+            backgroundColor='#A6B91A';
+        break;
+        case 'flying':
+            backgroundColor='#A98FF3';
+        break;
+        case 'dark':
+            backgroundColor='#705746';
+        break;
+        case 'fairy':
+            backgroundColor='#D685AD';
+        break;
+        case 'water':
+            backgroundColor='#6390F0';
+        break;
+    }
            // console.log("data: ", JSON.stringify(data[pokeKey]));
-            let pokemon = data[pokeKey];
+       
            // console.log("name " + pokemon.name );
             arr.push(
-                <div className="card" key={Math.floor(Math.random() * 1000000)}> 
+                <div className="card" style={{backgroundColor:`${backgroundColor}`}} key={Math.floor(Math.random() * 1000000)}> 
                     <img src={pokemon.img}></img>
-                    <h2>{pokemon.name}</h2>
+                    <h2>{pokemon.type}</h2>
                 </div>
             );
         }
--- src/componenets/DisplayPokemonList.js
+++ src/componenets/DisplayPokemonList.js
@@ -1,39 +1,9 @@
 import React,{useState,useEffect} from "react";
+import Card from "./Card";
 import Axios from "axios";
-import Form from "./componenets/form";
-import DisplayPokemonList from "./componenets/DisplayPokemonList";
-import Card from "./componenets/Card";
-function App() {
+const DisplayPokemonList=()=>{
 
-const[getPokemon,setGetPokemon]=useState('');
-
-//Data for Display
-const[pokemonName,setPokemonName]=useState('');
-const[pokemonType,setPokemonType]=useState('');
-const[pokemonFront,setPokemonFront]=useState('');
-
-//Data for cards
-const[pokemonCardContent,setPokemonCardContent]=useState([]);
-const pokemonGet=(name)=>{
-  setGetPokemon(name)
-};
-
-//Form to get pokemon but also a button for the cards to pass in the pokemons name to get the data and put it in display
-
-useEffect(()=>{
-
-Axios.get(`https://pokeapi.co/api/v2/pokemon/${getPokemon}`).then(response=>{
-setPokemonName(response.data.name);
-setPokemonType(response.data.types[0].type.name);
-setPokemonFront(response.data.sprites.front_default);}).catch((err)=>{console.log('error: ' +err);});
-},[getPokemon,pokemonName,pokemonType]);
-
-
-
-
-
-
-/*let start=20;
+let start=20;
 const[pokeDataURL,setPokeDataURL]=useState(`https://pokeapi.co/api/v2/pokemon?${start}`);
 const[pokeData,setPokeData]=useState('');
 
@@ -57,7 +27,7 @@ console.log(limit);
 /*for(let i=21;i<=limit;start=i++){
 arrayTest.push(i);
 }*/
-/*const last=arrayTest.pop()
+const last=arrayTest.pop()
 console.log(last)
 setPokeDataURL(`https://pokeapi.co/api/v2/pokemon?limit=${last}&offset=0`) 
 }
@@ -80,6 +50,7 @@ const poke=async (url)=>{const prom=Axios.get(url).then(response=>
           {
             name: response.data.name,
             img: response.data.sprites.front_default,
+            type:response.data.types[0].type.name,
           }
         ;
         // console.log('toreturn: ' + JSON.stringify(toreturn));
@@ -90,7 +61,7 @@ const poke=async (url)=>{const prom=Axios.get(url).then(response=>
     // console.log("toret = " + JSON.stringify(toret));
     return toret; 
     
-  };*/
+  };
 
   
  
@@ -98,15 +69,13 @@ const poke=async (url)=>{const prom=Axios.get(url).then(response=>
 
 //console.log(pokeData);
 //<button onClick={increaseLimit}>Increase</button>
-  return (
-  <div className="App">
-<Form pokemon={pokemonGet}></Form>
-<img src={pokemonFront}></img>
-<div>{pokemonName} {pokemonType}</div>
-<DisplayPokemonList></DisplayPokemonList>
+
+return(
+    <div>
+<Card data={pokeData}></Card>
+<button onClick={increaseLimit}>Increase</button>
     </div>
-  );
+)
 }
 
-
-export default App;
+export default DisplayPokemonList;
